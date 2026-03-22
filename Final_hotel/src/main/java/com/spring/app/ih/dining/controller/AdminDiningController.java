61641e2b118bede52d714b090d9d4e2861b5226e
user <user@DESKTOP-MRA1F63>
2026-03-22 10:22:21 +0900

    오전 10:22 2026-03-22 다이닝 매장별 관리자 페이지 리스트 불러오기 생성 커밋

--- Final_hotel/src/main/java/com/spring/app/ih/dining/controller/AdminDiningController.java
+++ Final_hotel/src/main/java/com/spring/app/ih/dining/controller/AdminDiningController.java
@@ -8,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.app.ih.dining.model.DiningDTO;
 import com.spring.app.ih.dining.model.DiningReservationDTO;
 import com.spring.app.ih.dining.model.ShopReservationStatDTO;
 import com.spring.app.ih.dining.service.DiningService;
@@ -216,6 +220,34 @@ public class AdminDiningController {
             e.printStackTrace();
             return -1; 
         }
+    }
+    
+    @GetMapping("/list")
+    public String adminDiningList(Model model) {
+    	Map<String, Object> paraMap = new HashMap<>();
+    	List<DiningDTO> diningList = diningservice.getAdminDiningList(paraMap);
+        model.addAttribute("diningList", diningList);
+        return "dining/admin/list"; 
+    }
+    
+    @GetMapping("/editdetail")
+    public String showEditForm(@RequestParam("dining_id") Long dining_id, Model model) {
+        DiningDTO dining = diningservice.getDiningDetail(dining_id);
+        model.addAttribute("dining", dining);
+        return "dining/admin/dininginfoedit"; 
+    }
+    
+    @PostMapping("/editdetail")
+    public String updateDiningInfo(@ModelAttribute DiningDTO diningDTO, RedirectAttributes redirectAttributes) {
+        int result = diningservice.updateDiningDetails(diningDTO);
+        
+        if(result > 0) {
+            redirectAttributes.addFlashAttribute("msg", "매장 정보가 성공적으로 수정되었습니다.");
+        } else {
+            redirectAttributes.addFlashAttribute("msg", "수정에 실패했습니다. 다시 시도해주세요.");
+        }
+        
+        return "redirect:/dining/detail/" + diningDTO.getDining_id();
     }
     
     
